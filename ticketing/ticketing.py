8d8817f396dfd9cff79d854bed5d26db46ffa63c
Kelby Valenti <kvalenti@vmware.com>
2017-10-26 13:45:45 -0400

    Use exchange_type

--- ticketing/ticketing.py
+++ ticketing/ticketing.py
@@ -216,7 +216,7 @@ def main():
 
     # Create a channel to subscribe to the incoming messages.
     sub_channel = connection.channel()
-    sub_channel.exchange_declare(exchange=RABBIT_EXCHANGE, type='direct', durable=True)
+    sub_channel.exchange_declare(exchange=RABBIT_EXCHANGE, exchange_type='direct', durable=True)
     sub_channel.queue_declare(queue=RABBIT_ROUTINGKEY)
     sub_channel.queue_bind(exchange=RABBIT_EXCHANGE,
                            queue=RABBIT_ROUTINGKEY)
@@ -235,5 +235,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
